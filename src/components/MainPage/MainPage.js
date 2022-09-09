2e1a6b36c3a5c459fd85f8097f9885430a297d08
pramods <srinivas.pramod0@gmail.com>
2022-09-09 16:59:34 +0530

    added gifs to ui

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,5 +1,10 @@
 import {useState, useRef} from 'react'
 import './MainPage.css'
+import leftswipe from '../../gifs/left_swipe.gif'
+import rightswipe from '../../gifs/right_swipe.gif'
+import stop from '../../gifs/stop.gif'
+import tumbs_down from '../../gifs/tumbs_down.gif'
+import tumbs_up from '../../gifs/tumbs_up.gif'
 
 function MainPage() {
     const [selectedFile, setSelectedFile] = useState(null)
@@ -31,22 +36,56 @@ function MainPage() {
 
     return (
         <>
-        <h1> Gesture Recognition App</h1>
-        <div className='upload'>
-            <form  onSubmit={predict_api}>
+            <h1> Gesture Recognition App</h1>
+            <div className='upload'>
+                <form  onSubmit={predict_api}>
+                    <div>
+                        <h3>Upload Gesture Here</h3>
+                    </div>
+                    <div className='uploadform'>
+                        <input ref={inputRef} type='file' name='video' accept="video/mp4,video/x-m4v,video/*" onChange={uploadVideo}/>
+                        <input type='submit'/>
+                    </div>
+                </form>
+            </div>
+            <h5>{prediction}</h5>
+            <div className='instruct'>
+                <h4 style={{textAlign:'left'}}>Instructions:</h4>
+                <p>
+                    1. Record a video with one of the following gestures right swipe, left swipe, stop, tumbs down and tumbs up
+                </p>
+                <p>
+                    2. Make sure the video length is below 3 seconds with less than 3Mb size
+                </p>
+                <p>
+                    3. Upload the video and click on submit
+                </p>
+                <p>
+                    4. App will recognise the gesture and returns the result
+                </p>
+            </div>
+            <div className='demogifs'>
                 <div>
-                    <h3>Upload Gesture Here</h3>
+                    <img src={leftswipe} alt='Left Swipe'/>
+                    <p>Left Swipe</p>  
                 </div>
-                <div className='uploadform'>
-                    <input ref={inputRef} type='file' name='video' accept="video/mp4,video/x-m4v,video/*" onChange={uploadVideo}/>
-                    <input type='submit'/>
+                <div>
+                    <img src={rightswipe} alt='Right Swipe'/>
+                    <p>Right Swipe</p>  
                 </div>
-            </form>
-        </div>
-        <h5>{prediction}</h5>
-        <div className='instruct'>
-            <h4>Instructions:</h4>
-        </div>
+                <div>
+                    <img src={stop} alt='Stop'/>
+                    <p>Stop</p>  
+                </div>
+                <div>
+                    <img src={tumbs_down} alt='Tumbs Down'/>
+                    <p>Tumbs Down</p>  
+                </div>
+                <div>
+                    <img src={tumbs_up} alt='Tumbs Up'/>
+                    <p>Tumbs Up</p>  
+                </div>
+            </div>
         </>
     )
 }
